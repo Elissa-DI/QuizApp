2621d259d3f523d0bd3823f98715f3dedef73f0d
Elissa <elissadusabe@gmail.com>
2023-04-25 20:45:31 +0200

    added

--- script.js
+++ script.js
@@ -51,7 +51,18 @@ function selectAnswer(e) {
 }
 
 function setStatusClass(element, correct) {
-    
+    clearStatusClass(element)
+    if(correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+
 }
 
 const questions = [
